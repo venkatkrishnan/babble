84d3916b7e4964be39442b6577f140ae6e3e38dd
venkatkrishnan <venkatkrishnan.u@hotmail.com>
2016-01-08 16:18:25 +0530

    basic structure

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var express = require('express');
 var app = express();
+
+var routes = require('./routes');
+var babble = require('./routes/babble');
+
+app.set('view engine', 'ejs');
+
+app.get('/', routes);
+app.get('/babble', babble);
+
+app.get('*', function(req, res) {
+  res.send('<h1>404 Error</h1>File not found');
+});
+
+var server = app.listen(8081, function() {
+  console.log("Server started");
+});
